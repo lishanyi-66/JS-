e536a77c9d4d6ca55b361bb042530d38d3a8fa87
lishanyi-66 <lishanyi18@gmail.com>
2021-12-01 00:46:28 +0800

    tools.js修改

--- tools.js
+++ tools.js
@@ -54,3 +54,45 @@ function move(obj,attr,target,speed,callback){
 		}
 	},30)
 }
+//定义一个函数，用来向一个元素中添加指定的class属性值
+/*
+*参数:
+* obj 要添加class属性的元素
+* cn 要添加的class值
+*/
+function addClass(obj,cn){
+	// 检查obj中是否含有cn
+	if(!hasClass(obj,cn)){
+		obj.className += " "+ cn;
+	}
+}
+
+/*
+*判断一个元素中是否含有指定的class属性值
+* 如果有class,则返回true,没有返回false
+*/
+function hasClass(obj,cn){
+	var reg = new RegExp("\\b" + cn  + "\\b");
+	return reg.test(obj.className);
+}
+
+/*
+*删除一个元素中的指定的class属性
+*/
+function remove(obj,cn){
+	// 创建一个正则表达式
+	var reg = new RegExp("\\b" + cn + "\\b");
+	obj.className = obj.className.replace(reg,"");
+}
+
+/*
+* toggleClass可以用来切换一个类
+*/
+function toggleClass(obj,cn){
+	//判断obj中是否含有cn
+	if(hasClass(obj.cn)){
+		removeClass(obj,cn);
+	}else{
+		addClass(obj,cn);
+	}
+}
